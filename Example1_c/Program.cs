47a8addbbf012bf9a7c2a78d272d1e85c7d6d72a
Marutaroriko <marusya.taroriko@mail.ru>
2021-12-24 01:03:23 +0300

    add Example1_c

---
+++ Example1_c/Program.cs
@@ -0,0 +1,45 @@
+﻿
+            //Добавить диалог с использованием switch демонстрирующий работу класса.
+            //Тарорико Мария
+            Complex z1 = new Complex(2, 3);
+            System.Console.WriteLine(z1);
+
+            Complex z2 = new (4, 5);
+            System.Console.WriteLine(z2);
+            
+            Complex z3;
+            z3 = z1 + z2;
+            System.Console.WriteLine($"Сумма равна = {z3}");
+
+            Complex z4;
+            z4 = z1 - z2;
+            System.Console.WriteLine($"Разность равна = {z4}");
+
+            Complex z5;
+            z5 = z1*z2;
+            System.Console.WriteLine($"Произведение равно = {z5}");
+
+
+    int i = int.Parse(System.Console.ReadLine());
+switch (i)
+{
+    case 3:
+    System.Console.WriteLine($"{z3}");
+    break;
+
+    case 4:
+    System.Console.WriteLine($"{z4}");
+    break;
+
+    case 5:
+    System.Console.WriteLine($"{z5}");
+    break;
+}
+
+
+
+  
+
+
+        
+
---
+++ Example1_c/SwitchCase.cs
@@ -0,0 +1,40 @@
+class Complex
+{
+    public int a; int b;
+    public Complex (int re, int im)
+    {
+       this.a = re;    
+       this.b = im;
+    }
+
+    public override string ToString()
+    {
+        string s;
+        if (b<0) s=$"{a} - {-b}i";
+        else s=$"{a} + {b}i";
+        return s;
+    }
+
+    public static Complex operator + (Complex z1, Complex z2)
+    {
+        Complex result;
+        result = new (z1.a+z2.a, z2.b+z1.b);
+        return result;
+    }
+
+    public static Complex operator - (Complex z1, Complex z2)
+    {
+        Complex minus;
+        minus = new (z1.a-z2.a, z1.b-z2.b);
+        return minus;
+    }
+
+    public static Complex operator * (Complex z1, Complex z2)
+    {
+        Complex multi;
+        multi = new Complex(z1.a*z2.a - z1.b*z2.b, z1.a*z2.b+z1.b*z2.a);
+        return multi;
+    }
+
+    
+}
