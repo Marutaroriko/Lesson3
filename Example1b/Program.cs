3954b4c8de05d999589f06e6be156e6df4e3f74e
Marutaroriko <marusya.taroriko@mail.ru>
2021-12-24 00:30:18 +0300

    add Example1b

---
+++ Example1b/Program.cs
@@ -0,0 +1,28 @@
+﻿//Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.
+//Тарорико Мария
+            Complex z1 = new Complex(2, 3);
+            System.Console.WriteLine(z1);
+
+            Complex z2 = new (4, 5);
+            System.Console.WriteLine(z2);
+            
+            Complex z3;
+            z3 = z1 + z2;
+            System.Console.WriteLine($"Сумма равна = {z3}");
+
+            Complex z4;
+            z4 = z1 - z2;
+            System.Console.WriteLine($"Разность равна = {z4}");
+
+            Complex z5;
+            z5 = z1*z2;
+            System.Console.WriteLine($"Произведение равно = {z5}");
+
+
+
+
+
+  
+
+
+        
